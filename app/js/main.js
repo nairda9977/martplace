8ad27bb8db23abf84abccd3212bc3918b8bac722
nairda9977 <nairda9977@gmail.com>
2020-04-20 02:16:27 +0100

    added slider

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".weekly__slider-inner").slick({
+         prevArrow:'<button class="slick-arrow slick-prev"><img src="images/weekly/arrow-left.png" alt=""></button>',
+        nextArrow:'<button class="slick-arrow slick-next"><img src="images/weekly/arrow-right.png" alt=""></button>'
+    })
+    
+    
+})
