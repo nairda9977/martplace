b11eb6de082533049805f3d49324dee113b944f3
nairda9977 <nairda9977@gmail.com>
2020-04-21 00:32:47 +0100

    added mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,15 @@ $(function(){
         nextArrow:'<button class="slick-arrow slick-next"><img src="images/weekly/arrow-right.png" alt=""></button>'
     })
     
+    $(function(){
+	var mixer = mixitup('.product__items');
+});
+    $(".followers__box").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1, 
+            prevArrow:'<button class="slick-arrow slick-prev"><img src="images/weekly/arrow-left.png" alt=""></button>',
+        nextArrow:'<button class="slick-arrow slick-next"><img src="images/weekly/arrow-right.png" alt=""></button>'
+    })
+    
     
 })
